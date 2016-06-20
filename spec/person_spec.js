e14bc31174573520e159fbd442d6a2ca86e7011b
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-21 00:11:16 +0200

    first commit: setup complete and person_spec.js begun

---
+++ spec/person_spec.js
@@ -0,0 +1,15 @@
+/*  spec/person_spec.js
+*/
+
+describe ("Person", function()) {
+  var person;
+
+  beforeEach(function() {
+    person = new Person({ height: 185, weight: 85 });
+  });
+
+
+
+
+
+}

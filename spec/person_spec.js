9a32d70927144c58eb8b2b8c4cb9d9afd631bd12
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-23 16:35:22 +0200

    added periods to replace underscores and nothingnesses in the person.weight and person.height and .toEqual

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -10,12 +10,12 @@ describe("Person", function() {
 
 
   it("should have weight of 90", function() {
-    expect(person_weight)toEqual(85);
+    expect(person.weight).toEqual(85);
   });
 
 
   it("should have height of 185", function() {
-    expect(person_height)toEqual(185);
+    expect(person.height).toEqual(185);
   });
 
 

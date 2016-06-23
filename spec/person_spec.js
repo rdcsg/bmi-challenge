eeb66ed3150647ed69dadfaedef9350f09866852
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-23 23:17:15 +0200

    going into the bmi accepting value dynamically after passing test with static values

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -5,17 +5,29 @@ describe("Person", function() {
   var person;
 
   beforeEach(function() {
-    person = new Person({ height: 185, weight: 85 });
+    person = new Person({ weight: 85, height: 185 });
   });
 
 
-  it("should have weight of 90", function() {
+  it("should have weight of 85", function() {
     expect(person.weight).toEqual(85);
   });
 
 
   it("should have height of 185", function() {
     expect(person.height).toEqual(185);
+  });
+
+
+  it("should calculate BMI value", function() {
+    person.calculate_bmi();
+    expect(person.bmiValue).toEqual(24.83)
+  });
+
+
+  it("should have a BMI message", function() {
+  person.calculate_bmi();
+  expect(person.bmiMessage).toEqual("Normal")
   });
 
 
--- src/person.js
+++ src/person.js
@@ -6,3 +6,8 @@ function Person(attr) {
   this.weight = attr.weight;
   this.height = attr.height;
 };
+
+Person.prototype.calculate_bmi = function() {
+  this.bmiValue = 24.83
+  this.bmiMessage = "Normal";
+}

1d87daada658bed84ecf1baab4aec9efae5f64d3
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-25 20:14:29 +0200

    adjusted the levels of bmi in bmi_calc

--- src/bmi_calculator.js
+++ src/bmi_calculator.js
@@ -15,13 +15,13 @@ function setBMIMessage (obj, value) {
   if (obj.bmiValue < 18.5) {
     obj.bmiMessage = "Underweight";
   }
-  if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
+  if (obj.bmiValue >= 18.5 && obj.bmiValue < 25) {
     obj.bmiMessage = "Normal";
   }
-  if (obj.bmiValue > 25 && obj.bmiValue < 30) {
+  if (obj.bmiValue >= 25 && obj.bmiValue < 30) {
     obj.bmiMessage = "Overweight";
   }
-  if (obj.bmiValue > 30) {
+  if (obj.bmiValue >= 30) {
     obj.bmiMessage = "Obese";
   }
 }

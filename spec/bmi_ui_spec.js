1b3dfb3812587cc30483149c559bf438569ae4d4
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-08 17:25:06 +0200

    actually add 'context' to both calculation cases by enclosing those tests in their own describe and beforeeach not just beforeeach

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -5,26 +5,35 @@ describe('BMI_UI - index.html', function() {
         jasmine.getFixtures().fixturesPath = '.';
         loadFixtures('index.html');
         $.holdReady(false);
-        $("input[type='radio']").get(0).checked = true;
-        $('#weight').val(85);
-        $('#height').val(185);
-        $('#calculate').trigger('click');
-    });
-
-    it("displays metric BMI Value", function() {
-        expect($('#display_value').text()).toBe('Your BMI is 24.84');
-    });
-
-    it("displays metric BMI Message", function() {
-        expect($('#display_message').text()).toBe('and you are Normal');
     });
 
 
-    beforeEach(function (){
-        $("input[type='radio']").get(1).checked = true;
-        $('#weight').val(187);
-        $('#height').val(72);
-        $('#calculate').trigger('click');
+    describe('imperial toggle off', function() {
+        beforeEach(function () {
+          $("input[type='radio']").get(0).checked = true;
+          $('#weight').val(85);
+          $('#height').val(185);
+          $('#calculate').trigger('click');
+        });
+
+        it("displays metric BMI Value", function() {
+            expect($('#display_value').text()).toBe('Your BMI is 24.84');
+        });
+
+        it("displays metric BMI Message", function() {
+            expect($('#display_message').text()).toBe('and you are Normal');
+        });
+    });
+
+
+
+    describe('imperial toggle on', function() {
+        beforeEach(function (){
+            $("input[type='radio']").get(1).checked = true;
+            $('#weight').val(187);
+            $('#height').val(72);
+            $('#calculate').trigger('click');
+        });
 
         it("displays imperial BMI Value", function() {
             expect($('#display_value').text()).toBe('Your BMI is 25.36');
@@ -34,4 +43,6 @@ describe('BMI_UI - index.html', function() {
             expect($('#display_message').text()).toBe('and you are Overweight');
         });
     });
+
+
 });
